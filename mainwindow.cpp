d36dcb5281f7a06c5d56bf8737260c1053bb929c
Your Name <you@example.com>
2024-10-28 15:59:51 +0800

    1.基本UI设计界面

---
+++ mainwindow.cpp
@@ -0,0 +1,33 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //显示长度和行号
+    statusLabel.setMaximumWidth(150);
+    statusLabel.setText("length: "+QString::number(0)+"  lines:   "+QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusLabel);
+
+    //显示行列
+    statusCursorLabel.setMaximumWidth(150);
+    statusCursorLabel.setText("Ln: "+QString::number(0)+"  Col:   "+QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusCursorLabel);
+
+    //设置作者
+    QLabel *author=new QLabel(ui->statusbar);
+    author->setText(tr("陈德城"));
+    ui->statusbar->addPermanentWidget(author);
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QLabel>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    //添加下面的状态栏，显示行号
+    QLabel statusCursorLabel;
+    QLabel statusLabel;
+
+};
+#endif // MAINWINDOW_H
